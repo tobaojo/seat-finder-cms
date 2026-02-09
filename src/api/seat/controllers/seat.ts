b49bb9251e24a3b96fcb76cc668a9fcfb0fdab69
Toba Ojo <toba.ojo@mav-systems.com>
2026-02-09 15:44:51 +0000

    feat: add home-page and seat content types with controllers, routes, and services; update CORS middleware configuration

---
+++ src/api/seat/controllers/seat.ts
@@ -0,0 +1,7 @@
+/**
+ * seat controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::seat.seat');
---
+++ src/api/seat/routes/seat.ts
@@ -0,0 +1,7 @@
+/**
+ * seat router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::seat.seat');
